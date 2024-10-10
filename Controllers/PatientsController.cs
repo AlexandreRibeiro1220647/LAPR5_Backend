861171828216ba98be34d1221f96c3e5429909bc
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-10-10 10:47:08 +0100

    #25 Base (TodoAPI) Project Updates

---
+++ Controllers/PatientsController.cs
@@ -0,0 +1,14 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientsController : ControllerBase
+    {
+        public PatientsController()
+        {
+        }
+    }
+
+}
---
+++ Models/UserContext.cs
@@ -0,0 +1,19 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace TodoApi.Models;
+
+public class UserContext : DbContext
+{
+    public UserContext(DbContextOptions<UserContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; } 
+     protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .ToTable("Users"); 
+        }
+    
+}
