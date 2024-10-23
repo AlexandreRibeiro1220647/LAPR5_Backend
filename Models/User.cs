083facb27afcaef9cb16503457ca01691aae0d9f
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-10-23 15:19:36 +0100

    #3 Small Update regarding User Domain class

--- Models/User.cs
+++ Models/User.cs
@@ -5,7 +5,7 @@ public class User
     public long Id { get; set; }
     public string? Email { get; set; }
     public string? UserName { get; set; }
-    public string? PhoneNumber { get; set; }
+    public string? Phone { get; set; }
     public string? FirstName { get; set; }
     public string? LastName { get; set; }
     public string? FullName { get; set; }
