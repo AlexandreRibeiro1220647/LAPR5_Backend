173390f0bb1764e95b173dd339ec092d4ea965ce
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-10-24 17:22:45 +0100

    #1 Implemented User DTOs

---
+++ DTOs/User/UserDTO.cs
@@ -0,0 +1,24 @@
+﻿namespace TodoApi.DTOs.User;
+
+public class UserDTO
+{
+    public string Id { get; set; }
+    public string Name { get; set; }
+    public string Email { get; set; }
+    public string Role { get; set; }
+
+    
+    public UserDTO()
+    {
+        
+    }
+    
+    public UserDTO(string id, string name, string email, string role)
+    {
+        Id = id;
+        Name = name;
+        Email = email;
+        Role = role;
+    }
+    
+}
