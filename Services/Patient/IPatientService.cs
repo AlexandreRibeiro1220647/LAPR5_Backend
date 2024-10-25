4260f1b7845160efc07d316fe9df539dc6b80a1f
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-10-25 22:26:00 +0100

    #11 Updated IPatientService

--- Services/Patient/IPatientService.cs
+++ Services/Patient/IPatientService.cs
@@ -7,4 +7,10 @@ public interface IPatientService {
     Task<PatientDTO> UpdatePatientAsync(Guid id, UpdatePatientDTO dto);
     Task<List<PatientDTO>> GetAllPatients();
     Task<bool> DeletePatientByEmailAsync(string email);
+    Task<PatientDTO> GetPatientByEmailAsync(string email);
+    Task<PatientDTO> GetPatientByIdAsync(Guid id);
+    Task<List<PatientDTO>> GetPatientsByNameAsync(string name);
+    Task<List<PatientDTO>> GetPatientsByContactInformationAsync(string contact);
+    Task<List<PatientDTO>> GetPatientsByGenderAsync(Gender gender);
+    Task<List<PatientDTO>> GetPatientsByDateOfBirthAsync(DateOnly dateOfBirth);
 }
