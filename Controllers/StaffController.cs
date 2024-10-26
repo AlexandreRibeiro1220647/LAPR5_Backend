fa0bbd710173973edbb3176a0dbf1634d0a2ed25
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-10-26 19:12:07 +0100

    #14 Update Staff Controller

--- Controllers/StaffController.cs
+++ Controllers/StaffController.cs
@@ -113,12 +113,12 @@ namespace TodoApi.Controllers
             }
         }
 
-        [HttpPatch("updatestatus/{id}")]
-        public async Task<IActionResult> UpdateStaffStatus(Guid id, [FromBody] UpdateStaffDTO dto)
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> InactivateStaff(Guid id, [FromBody] UpdateStaffDTO dto)
         {
             try
             {
-                var updatedStaffStatusDto = await _staffService.UpdateStaffStatus(id, dto);
+                var updatedStaffStatusDto = await _staffService.InactivateStaff(id, dto);
                 return Ok(updatedStaffStatusDto);
             }
             catch (Exception e)
