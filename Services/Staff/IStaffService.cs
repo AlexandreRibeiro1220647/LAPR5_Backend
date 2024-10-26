84748168ab282026be5aaf0f5100832d7a72a676
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-10-26 22:23:39 +0100

    Revert "#14 Update Staff Services"
    This reverts commit 641b16089704eb47c38e27fec6d80b544b8207a1.

--- Services/Staff/IStaffService.cs
+++ Services/Staff/IStaffService.cs
@@ -8,7 +8,7 @@ namespace TodoApi.Services
         Task<StaffDTO> CreateStaff(CreateStaffDTO dto);
         Task<List<StaffDTO>> GetStaff();
         Task<StaffDTO> UpdateStaff(Guid id, UpdateStaffDTO dto);
-        Task<StaffDTO> InactivateStaff(Guid id, UpdateStaffDTO dto);
+        Task<StaffDTO> UpdateStaffStatus(Guid id, UpdateStaffDTO dto);
         Task<List<StaffDTO>> GetStaffByName(string name);
         Task<List<StaffDTO>> GetStaffByEmail(string email);
         Task<List<StaffDTO>> GetStaffBySpecialization(string specialization);
