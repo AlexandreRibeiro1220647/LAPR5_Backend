edfb398df9735f5ce8295534ccfb68dda981cfa6
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-10-26 17:53:50 +0100

    #Update Staff Repository

--- obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TodoApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cce87ae03e70bc1331a209cb0e3d439b3a298738")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6269bae3294235d2d6f579bfa7165480333ca010")]
 [assembly: System.Reflection.AssemblyProductAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TodoApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
