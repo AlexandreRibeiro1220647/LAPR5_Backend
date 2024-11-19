bafd5628210b7a5e1a8ea736491715f7fcf373d9
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-11-19 22:44:00 +0000

    #40 Update IPatientRepository

--- Infrastructure/Patient/IPatientRepository.cs
+++ Infrastructure/Patient/IPatientRepository.cs
@@ -6,7 +6,6 @@ namespace TodoApi.Infrastructure.Patient;
 public interface IPatientRepository : IRepository<Models.Patient.Patient, MedicalRecordNumber> {
     void DeletePatient(Models.Patient.Patient patient);
     Task<Models.Patient.Patient?> GetByIdAsync(MedicalRecordNumber id);
-    Task<List<Models.Patient.Patient>> GetByContactInformationAsync(string contact);
     Task<bool> ExistsAsync(MedicalRecordNumber patientId);
     Task<List<Models.Patient.Patient>> SearchAsync(string? contact, Gender? gender, DateOnly? dateOfBirth);
     Task<List<Models.Patient.Patient>> GetByUserAsync(TodoApi.DTOs.User.UserDTO user);
