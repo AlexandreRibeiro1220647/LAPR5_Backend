8d2d614baa5b2ded9af8b01dfc47f7cafe113afb
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-11-21 01:06:06 +0000

    Update IStaffMapper.cs

--- Mappers/Staff/IStaffMapper.cs
+++ Mappers/Staff/IStaffMapper.cs
@@ -1,9 +1,11 @@
 using TodoApi.Models.Staff;
 using TodoApi.DTOs;
 
-namespace TodoApi.Mappers
+namespace TodoApi.Mappers;
+
+public interface IStaffMapper
 {
-    public interface IStaffMapper : IMapper<Staff, StaffDTO, CreateStaffDTO>
-    {
-    }
+    Staff toEntity(CreateStaffDTO dto, TodoApi.DTOs.User.UserDTO user);
+    Staff ToEntity(StaffDTO dto,TodoApi.DTOs.User.UserDTO user);
+    StaffDTO ToDto(Staff entity,TodoApi.DTOs.User.UserDTO user);
 }
