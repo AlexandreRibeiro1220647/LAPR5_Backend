aba6f44e7d1236ffcb1fa653aec5c85692fa1fc2
FernandoSilva119166611 <1191666@isep.ipp.pt>
2024-11-21 01:06:25 +0000

    Update StaffDTO.cs

--- DTOs/Staff/StaffDTO.cs
+++ DTOs/Staff/StaffDTO.cs
@@ -5,24 +5,22 @@ namespace TodoApi.DTOs
     public class StaffDTO
     {
         public string Specialization { get; set; }
-        public string FullName { get; set; }
         public string LicenseNumber { get; set; }
         public string Phone { get; set; }
-        public string Email { get; set; }
         public List<Slot> AvailabilitySlots { get; set; }
         public StaffStatus Status { get; set; }
+        public TodoApi.DTOs.User.UserDTO User {get; set; }
 
         public StaffDTO() { }
 
-        public StaffDTO(string fullName, string specialization, string licenseNumber, string email, string phone, List<Slot> availabilitySlots, StaffStatus status)
+        public StaffDTO(string specialization, string licenseNumber, string phone, List<Slot> availabilitySlots, StaffStatus status,TodoApi.DTOs.User.UserDTO user)
         {
-            this.FullName = fullName;
             this.Specialization = specialization;
             this.LicenseNumber = licenseNumber;
-            this.Email = email;
             this.Phone = phone;
             this.AvailabilitySlots = availabilitySlots;
             this.Status = status;
+            User = user;
         }
     }
 }
