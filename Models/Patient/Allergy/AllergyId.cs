4e69d132710c59ef2a2b98414e0bc781348cac8d
AlexandreRibeiro1220647 <162116947+AlexandreRibeiro1220647@users.noreply.github.com>
2024-12-23 22:59:53 +0000

    #70 Allergy Domain

---
+++ Models/Patient/Allergy/AllergyId.cs
@@ -0,0 +1,39 @@
+﻿using System.Text.Json.Serialization;
+using TodoApi.Models.Shared;
+
+namespace TodoApi.Models.Patient {
+    public class AllergyId : EntityId 
+    {
+        // Property to store the value for EF Core mapping
+        [JsonPropertyName("value")]
+        public Guid Value { get; private set; }
+        
+        [JsonConstructor]
+        public AllergyId(Guid value) : base(value)
+        {
+            this.Value = value;
+        }
+
+        public AllergyId(string value) : base(new Guid(value))
+        {
+            this.Value = new Guid(value);
+        }
+
+
+        protected override object createFromString(string text)
+        {
+            return new Guid(text);
+        }
+
+        public override string AsString()
+        {
+            return Value.ToString();
+        }
+
+        public Guid AsGuid()
+        {
+            return Value;
+        }
+
+    }
+}
